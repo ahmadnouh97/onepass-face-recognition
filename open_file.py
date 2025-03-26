b59f04774e6f2ea89a6426309b5e9afefd9c9dfe
Ahmad Nouh <ahmadnouh428@gmail.com>
2025-03-27 01:04:34 +0300

    chore:  remove unnecessary files

--- open_file.py
+++
@@ -1,7 +0,0 @@
-import os
-import subprocess
-
-
-
-subprocess.run(["start", os.path.join(os.path.dirname(__file__), "captured_photo_1.jpg")], shell=True)
-subprocess.run(["start", os.path.join(os.path.dirname(__file__), "captured_face_1_0.jpg")], shell=True)
